161254330d495b6a4a4bfcb565c499db288bed8d
Swift984 <79926526+Swift984@users.noreply.github.com>
2022-03-23 09:18:29 -0500

    Add files via upload

--- Board.java
+++ Board.java
@@ -6,10 +6,71 @@ import javax.sound.*;
 import javax.swing.*;
 import java.util.*;
 import java.io.*;
+import javax.imageio.*;
 
 public class Board extends JPanel implements Runnable , KeyListener
 {
+	private File BoardJPG;
 
+    private int MouseX;
+    private int MouseY;
+	
+	
+	
+	
+	public Board()
+	{
+		setBackground(Color.WHITE);
+		
+		BoardJPG = new File("gameboard.jpg");
+		
+		addKeyListener( this );
+		setFocusable( true );
+		
+		new Thread(this).start();
+	}
+	
+	
+	public void paint( Graphics window )
+	{
+		window.clearRect( 0,0, 1350, 1350);
+		try {
+			window.drawImage(ImageIO.read(BoardJPG), 0, 0, 1350, 1350, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		MouseX = MouseInfo.getPointerInfo().getLocation().x;
+		MouseY = MouseInfo.getPointerInfo().getLocation().y;
+		window.setColor(Color.BLACK);
+		window.drawString("Mouse  coordinates " + "(" + MouseX + "   " + MouseY + ")", 250, 30 );
+		
+		
+	}
+	
+	
+	
+	public void run()
+	{
+
+		try
+		{
+			while( true )
+			{	
+			   Thread.sleep(50);
+			   repaint();
+			}
+		}
+		catch( Exception e )
+		{
+			
+		}
+
+	}
+	
+	
+	
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
@@ -24,12 +85,6 @@ public class Board extends JPanel implements Runnable , KeyListener
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
