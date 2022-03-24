4788a4b9bb68389302344703fe8271edb7806d46
Swift984 <79926526+Swift984@users.noreply.github.com>
2022-03-24 09:20:03 -0500

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.sound.*;
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class Board extends JPanel implements Runnable , KeyListener
+{
+	private File BoardJPG;
+	
+    private int MouseX;
+    private int MouseY;
+	
+	
+	
+	
+	public Board()
+	{
+		setBackground(Color.WHITE);
+		
+		BoardJPG = new File("gameboard.jpg");
+		
+		addKeyListener( this );
+		setFocusable( true );
+		
+		new Thread(this).start();
+	}
+	
+	
+	public void paint( Graphics window )
+	{
+		window.clearRect( 0,0, 1350, 1350);
+		try {
+			window.drawImage(ImageIO.read(BoardJPG), 0, 0, 1350, 1350, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		MouseX = MouseInfo.getPointerInfo().getLocation().x-getLocationOnScreen().x;
+		MouseY = MouseInfo.getPointerInfo().getLocation().y-getLocationOnScreen().y;
+		window.setColor(Color.BLACK);
+		window.drawString("Mouse  coordinates " + "(" + MouseX + "   " + MouseY + ")", 250, 30 );
+		
+		
+	}
+	
+	
+	
+	public void run()
+	{
+		try
+		{
+			while( true )
+			{	
+			   Thread.sleep(50);
+			   repaint();
+			}
+		}
+		catch( Exception e )
+		{
+			
+		}
+	}
+	
+	
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
