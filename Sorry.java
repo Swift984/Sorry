49266becd2b95a2633ee605c3fc779b6092b7d64
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-03-24 08:35:22 -0500

    Add files via upload

--- Sorry.java
+++ Sorry.java
@@ -7,16 +7,15 @@ import java.io.*;
 
 public class Sorry extends JFrame
 {
-	private static final int WIDTH = 1366;
-	private static final int HEIGHT = 1390;
+	private static final int WIDTH = 1920;
+	private static final int HEIGHT = 1920;
 	
 	public Sorry() throws IOException
 	{
 		super("SORRY");
 
 		setSize(WIDTH,HEIGHT);
-		setLocation(600,0);
-		setIconImage(new ImageIcon("pawnBLACK.png").getImage());
+		setLocation(325,0);
 		
 		getContentPane().add(new Board() );
 
@@ -25,8 +24,27 @@ public class Sorry extends JFrame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
+	public void players() {
+		System.out.println("How many players?");
+		Scanner input = new Scanner(System.in);
+		int p = input.nextInt();
+			if(p > 0 && p < 5)
+			{
+				System.out.println(p);
+			}
+			else if(p < 1 || p > 4 )
+			{
+				System.out.println("Please pick a number of players between 1 and 4");
+					players();
+			}
+	}
 	public static void main( String args[] ) throws IOException
 	{
 		Sorry run = new Sorry();
+		run.players();
+		Deck deck = new Deck();
+		if(deck.size() == 0)
+			deck.Shuffle();
+		
 	}
 }
