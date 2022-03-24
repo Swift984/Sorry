16b087093ac9e098f93b9cd189a613187f35589d
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-03-24 08:21:55 -0500

    Add files via upload

--- Piece.java
+++ Piece.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Piece {
 	public int color;
@@ -16,31 +17,57 @@ public class Piece {
 	public void move(int s)
 	{
 		move = 0;
+		if(s == 0)
+		{
+			System.out.println("If you have a pawn in start you can switch it's place with an opponants pawn on the board. Otherwise skip your turn.");
+		}
 		 if(s == 1)
 			 move = 1;
-		 System.out.println("ould you like to get a pawn out of start?");
+		 System.out.println("Would you like to get a pawn out of start?");
+		 
 		 if(s == 2)
 			 move = 2;
+		 System.out.println("Would you like to get a pawn out of start?");
+		 
 		 if(s == 3)
-			 move = 1;
+			 move = 3;
+		 
 		 if(s == 4)
-			 move = 1;
+			 move = -4;
+		 
 		 if(s == 5)
-			 move = 1;
+			 move = 5;
+		 
 		 if(s == 6)
-			 move = 1;
+			 move = 6;
+		 
 		 if(s == 7)
-			 move = 1;
+			 move = 7;
+		 System.out.println("Would you like to split the movement between 2 pawns");
+		 
 		 if(s == 8)
-			 move = 1;
+			 move = 8;
+		 
 		 if(s == 9)
-			 move = 1;
+			 move = 9;
+		 
 		 if(s == 10)
-			 move = 1;
+			 System.out.println("Would you like to get A move 10, or B move backwards 1?");
+		 		Scanner input = new Scanner(System.in);
+		 		String p = input.next();
+		 		while(p != null)
+		 		{
+		 			if(p == "A" )
+		 				move = 10;
+		 			if(p == "B" )
+		 				move = -1;
+		 		}
 		 if(s == 11)
-			 move = 1;
+			 move = 11;
+		 	System.out.println("Would you like to switch places with another players pawn?");
+		 	
 		 if(s == 12)
-			 move = 1;
+			 move = 12;
 		 
 												 
 			 
--- player.java
+++ player.java
@@ -2,10 +2,16 @@
 public class Player {
 	public int color;
 	public int pos;
+	public int steps;
 	
-	public Player(int c) {
-		
+	public Player(int c, int p, int s) {
+		c = color;
+		p = pos;
+		s = steps;
 	}
 	
+	public void draw(int s) {
+		
+	}
 	
 }
