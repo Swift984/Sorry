a29fefcfcb683a69d4bd22b96a6992edd1211d7e
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-03-30 08:36:32 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -18,8 +18,11 @@ public class Board extends JPanel implements Runnable , KeyListener
 	
     private int MouseX;
     private int MouseY;
+    
+    private int wowX;
+    private int wowY;
 	
-	
+    private Piece R1;
 	
 	
 	public Board()
@@ -31,6 +34,9 @@ public class Board extends JPanel implements Runnable , KeyListener
 		Blue = new File("pawnBLUE.png");
 		Yellow = new File("pawnYELLOW.png");
 		Green = new File("pawnGREEN.png");
+		wowX = 0;
+		wowY = 0;
+		R1 = new Piece("Red", 11, 14);
 		
 		addKeyListener( this );
 		setFocusable( true );
@@ -44,10 +50,13 @@ public class Board extends JPanel implements Runnable , KeyListener
 		window.clearRect( 0,0, 1280, 1280);
 		try {
 			window.drawImage(ImageIO.read(BoardJPG), 0, 0, 1280, 1280, null);
+			//window.drawImage(ImageIO.read(Red), 37+(wowX*75)+(wowX/3), 37+(wowY*75)+(wowY/3), 75, 75, null);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		drawPiece(R1, window);
 		
 		MouseX = MouseInfo.getPointerInfo().getLocation().x-getLocationOnScreen().x;
 		MouseY = MouseInfo.getPointerInfo().getLocation().y-getLocationOnScreen().y;
@@ -57,7 +66,17 @@ public class Board extends JPanel implements Runnable , KeyListener
 		
 	}
 	
-	
+	public void drawPiece(Piece p, Graphics g)
+	{
+		
+		try {
+			g.drawImage(ImageIO.read(Red), 37+(wowX*75)+(wowX/3), 37+(wowY*75)+(wowY/3), 75, 75, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	public void run()
 	{
@@ -84,8 +103,16 @@ public class Board extends JPanel implements Runnable , KeyListener
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+	public void keyReleased(KeyEvent e)
+	{
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT )
+			wowX = wowX+1;
+		if(e.getKeyCode() == KeyEvent.VK_LEFT )
+			wowX = wowX-1;
+		if(e.getKeyCode() == KeyEvent.VK_DOWN )
+			wowY = wowY+1;
+		if(e.getKeyCode() == KeyEvent.VK_UP )
+			wowY = wowY-1;
 		
 	}
 
